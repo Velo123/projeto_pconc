4adbcb350c171ff5e88e83db7771dd549a63a510
davidfma2003 <davidfma2003@gmail.com>
2022-12-14 13:53:20 +0000

    Co-authored-by: Velo123 <Velo123@users.noreply.github.com>

--- ap_prl2/input.c
+++ ap_prl2/input.c
@@ -1,14 +1,30 @@
 #include"input.h"
 
+/******************************************************************************
+ * input_directorys()
+ *
+ * Arguments: filename - string com o nome do ficheiro que contem os diretórios com as
+ *                       imagens a tratar
+ * 
+ * Returns: vector de strings com os diretorios de todas as imagens a resolver com o ultimo endereço de 
+ *          string a nulo
+ * Side-Effects: none
+ *
+ * Description: esta função lê todo o ficheiro de diretorios conta o numero de diretorios existentes
+ *              válidos e guarda-os num vetor de diretórios que retorna
+ *
+ *****************************************************************************/
+
 char** input_directorys(char* filename){
     char directory[700],**directorys;
     int n_images=0,i=0;
     FILE *fin=fopen(filename,"r");
-
+    //comfirma a existencia do ficheiro
     if (fin==NULL){
         printf("Error opening the file:%s\n",filename);
         return NULL;
     }
+    //percorre todo o ficheiro e conta o numero de diretorios válidos
     while (!feof(fin)){
         fscanf(fin,"%s\n",directory);
         if(strstr(directory,".png")!=NULL && strcmp(directory,"watermark.png")!=0){
@@ -17,14 +33,16 @@ char** input_directorys(char* filename){
     }
     
     fseek(fin,0,SEEK_SET);
-
+    //Aloca a memória nesseçária para guardar todos os diretorios 
     directorys=(char**)malloc((n_images+1)*sizeof(char*));
     if (directorys==NULL){
         fclose(fin);
         return NULL;
     }
+    //marca a ultima casa do vetor a nulo
     directorys[n_images]=NULL;
-    
+
+    ///percorre todo o ficheiro de novo e guarda os dirétorios válidos
     while (!feof(fin)){
         fscanf(fin,"%s\n",directory);
         if(strstr(directory,".png")==NULL || strcmp(directory,"watermark.png")==0)
